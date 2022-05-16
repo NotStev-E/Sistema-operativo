015ccff1dd47efd84316ff1b6d76e3895749fa6f
steve <stevegomitas3@gmail.com>
2022-05-15 23:42:57 -0600

    Se mejoro el sistema operativo con javascript

--- js/main.js
+++ js/main.js
@@ -1,8 +1,61 @@
-var barra = document.getElementById('barraTareas');
-var windows = document.getElementById('windows');
-
 function activar(){
-    windows.classList.add("activar");
+    var windows = document.getElementById('botonWindows');
+    var inicio = document.getElementById('inicio');
+    var botonWindows2 = document.getElementById('botonWindows2');
+
+    inicio.classList.add("activar");
+    windows.classList.add("desactivar");
+    botonWindows2.classList.add("activar");
+}
+function desactivar(){
+    var windows = document.getElementById('botonWindows');
+    var inicio = document.getElementById('inicio');
+    var botonWindows2 = document.getElementById('botonWindows2');
+
+    inicio.classList.remove("activar");
+    windows.classList.remove("desactivar");
+    botonWindows2.classList.remove("activar");
+}
+function abrirVentana(){
+    var botonCarpeta = document.getElementById('botonArchivo');
+    var botonCarpeta2 = document.getElementById('botonArchivo2');
+    var ventana = document.getElementById('ventana');
+
+    botonCarpeta.classList.add("desactivar");
+    botonCarpeta2.classList.add("activar");
+    ventana.classList.add("activar");
+}
+function cerarVentana(){
+    var botonCarpeta = document.getElementById('botonArchivo');
+    var botonCarpeta2 = document.getElementById('botonArchivo2');
+    var ventana = document.getElementById('ventana');
+
+    botonCarpeta.classList.remove("desactivar");
+    botonCarpeta2.classList.remove("activar");
+    ventana.classList.remove("activar");
 }
 
-windows.addEventListener('click', activar);
+function reloj(){
+    var reloj = document.getElementById('botonReloj');
+    var horaYFecha = document.getElementById('horaYFecha');
+
+    horaYFecha.classList.add("activar");
+}
+function noVerReloj(){
+    var reloj = document.getElementById('botonReloj');
+    var horaYFecha = document.getElementById('horaYFecha');
+
+    horaYFecha.classList.remove("activar");
+}
+function apagar(){
+    var iconoApagar = document.getElementById('iconoApagar');
+    var apagando = document.getElementById('apagar');
+
+    apagando.classList.add("activar");
+}
+function encender(){
+    var iconoApagar = document.getElementById('iconoApagar');
+    var encender = document.getElementById('apagar');
+
+    encender.classList.remove("activar");
+}
